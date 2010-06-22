ac0997f9929e70fa0c81e412384f27ed402d1f33
Alex <maccman@gmail.com>
2010-06-22 14:13:05 -0700

    Helper function to determine if Syncro's Scriber is active

--- lib/syncro/scriber.rb
+++ lib/syncro/scriber.rb
@@ -9,5 +9,10 @@ module Syncro
       Observer.disable(&block)
     end
     module_function :disable
+    
+    def active?
+      !!Observer.from_client
+    end
+    module_function :active?
   end
 end
